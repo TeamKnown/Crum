50dfb68fd9e3030d8439010ab21e643dc5cfa36d
mczernyk <mczernyk@gmail.com>
2020-03-19 22:33:06 -0400

    adding store etc

--- server/api/crumInstances.js
+++ server/api/crumInstances.js
@@ -13,6 +13,24 @@ router.get('/', async (req, res, next) => {
 })
 
 // http://localhost:19001/api/cruminstances/nearme?radium=1000&latitudeIdx=407074&longitudeIdx=-740000
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newCrumInstance = await CrumInstance.create(req.body, {
+      include: [
+        {
+          model: Crum
+        }
+      ]
+    })
+    if (newCrumInstance) {
+      res.json(newCrumInstance)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/nearme', async (req, res, next) => {
   try {
     const crumInstances = await CrumInstance.findNearMe(
