80c00e86b30fd5bdc1955672714b4468bfff337e
mczernyk <mczernyk@gmail.com>
2020-03-24 20:48:17 -0400

    delete 404

--- server/auth/index.js
+++ server/auth/index.js
@@ -34,6 +34,7 @@ router.post('/signup', async (req, res, next) => {
 router.post('/logout', (req, res) => {
   req.logout()
   req.session.destroy()
+  res.redirect('/signup')
 })
 
 router.get('/me', (req, res) => {
