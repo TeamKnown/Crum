5dd7a71dfe001f6094f4b9972eadb68e012930eb
chaohuichen <chaohuichen@github.com>
2020-03-25 21:20:19 -0400

    fixed merge

--- client/components/UserSettingsModal.js
+++ client/components/UserSettingsModal.js
@@ -1,11 +1,8 @@
 import * as React from 'react'
 import {connect} from 'react-redux'
 import {getSingleUser} from '../store'
-import {logout} from '../store/user'
 import {updateUserThunk} from '../store/'
-import EditUserModalForm from './EditUserModalForm'
-import ViewCrumsModal from './ViewCrumsModal'
-import UserSettingsModal from './UserSettingsModal'
+// import {} from '../store/'
 
 import {
   Platform,
@@ -17,148 +14,117 @@ import {
   ImageBackground,
   TextInput,
   TouchableOpacity,
+  Modal,
   TouchableHighlight,
   Alert
 } from 'react-native'
 
-class UserProfile extends React.Component {
+class UserSettingsModal extends React.Component {
   constructor(props) {
     super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  state = {
+    visible: false
   }
 
-  async handleSignOut() {
-    await this.props.logout()
+  setModalVisible(visible) {
+    this.setState({
+      visible: visible
+    })
   }
 
+  handleSubmit(userId, info) {
+    this.props.editUser(userId, info)
+  }
   render() {
     const {user} = this.props
-    console.log('state', this.state)
     return (
-      <View style={styles.main}>
-        <View style={styles.topContainer}>
-          <Image
-            source={require('../../public/defaultProfile1.png')}
-            style={{
-              width: 220,
-              height: 220,
-              marginRight: 10,
-              marginBottom: 12,
-              marginTop: '15%'
-            }}
-          />
-        </View>
-        <View style={styles.container}>
-          <View style={styles.bottomContainer}>
-            <View style={styles.editButtons}>
-              <Text
-                style={{
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: 16,
-                  fontWeight: 'bold'
+      <View style={styles.btnCrum}>
+        <TouchableOpacity
+          onPress={() => {
+            this.setModalVisible(true)
+          }}
+        >
+          <Text style={{color: 'white'}} title="settings">
+            s e t t i n g s
+          </Text>
+        </TouchableOpacity>
+        <Modal
+          animationType="none"
+          transparent={false}
+          visible={this.state.visible}
+          onRequestClose={() => {
+            Alert.alert('Modal closed')
+          }}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modal}>
+              <Text>e d i t</Text>
+              <Text>u s e r</Text>
+
+              <TouchableOpacity
+                style={styles.btnDrop}
+                onPress={() => {
+                  this.setModalVisible(!this.state.visible)
                 }}
               >
-                u s e r n a m e
-              </Text>
-              <View>
-                <EditUserModalForm />
-              </View>
+                <Text style={{color: '#19ae9f'}} title="edit">
+                  d o n e
+                </Text>
+              </TouchableOpacity>
             </View>
-            {user.id ? (
-              <Text
-                style={{
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: 16
-                }}
-              >
-                {user.userName}
-              </Text>
-            ) : (
-              <Text
-                style={{
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginTop: 16
-                }}
-              >
-                P L A C E H O L D E R
-              </Text>
-            )}
-            <ViewCrumsModal />
           </View>
-          <View style={styles.buttons}>
-            <TouchableOpacity
-              style={styles.btnLogout}
-              onPress={() => this.handleSignOut()}
-            >
-              <Text style={{color: 'white'}}>logout</Text>
-            </TouchableOpacity>
-            <UserSettingsModal style={styles.btnCrum} />
-          </View>
-        </View>
+        </Modal>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  main: {
-    height: '100%',
-    width: '100%',
-    paddingBottom: '4%'
-  },
-  topContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  bottomContainer: {
-    flex: 1,
+  modal: {
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'grey',
+    borderColor: '#7c1e9f',
     width: '90%',
-    paddingBottom: '4%'
+    height: '70%',
+    shadowColor: 'grey',
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    borderRadius: 10,
+    marginTop: 100
   },
-  buttons: {
+
+  modalContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  btnDrop: {
+    height: 60,
     width: '90%',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    backgroundColor: 'white',
+    borderColor: '#19ae9f',
+    borderWidth: 2,
+    textAlign: 'center',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 8
   },
-  editButtons: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'space-around'
-  },
-  btnLogout: {
+  btnCrum: {
     width: '47%',
     height: 60,
-    backgroundColor: '#19ae9f',
+    backgroundColor: '#7c1e9f',
     textAlign: 'center',
     borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 10
-  },
-
-  input: {
-    height: 60,
-    width: '90%',
-    borderRadius: 10,
-    borderColor: 'grey',
-    backgroundColor: 'white',
-    borderWidth: 2,
-    alignItems: 'center',
-    padding: 8,
-    margin: 8
   }
 })
 
@@ -170,9 +136,10 @@ const mapState = state => {
 
 const mapDispatch = dispatch => {
   return {
-    getSingleUser: id => dispatch(getSingleUser(id)),
-    logout: () => dispatch(logout())
+    editUser: (userId, info) => {
+      dispatch(updateUserThunk(userId, info))
+    }
   }
 }
 
-export default connect(mapState, mapDispatch)(UserProfile)
+export default connect(mapState, mapDispatch)(UserSettingsModal)
